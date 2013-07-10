8d01dd547e1018bb82675deec2157ad4756c6cf6
Kjell Andreassen <andreassen.kjell@gmail.com>
2013-07-10 12:07:00 +0200

    Use use-package for emacs dyalog-mode.

--- elisp/my-apl.el
+++ elisp/my-apl.el
@@ -1,19 +1,22 @@
 ;;(set-frame-font "APL385 Unicode 10")
 
-;; Dyalog-mode
-(autoload 'dyalog-mode "dyalog-mode" "Edit Dyalog APL" t)
-(add-to-list 'auto-mode-alist '("\\.apl\\'" . dyalog-mode))
-(add-to-list 'auto-mode-alist '("\\.dyalog$" . dyalog-mode))
-(add-to-list 'file-coding-system-alist '("\\.apl\\'" . utf-8))
-(add-to-list 'file-coding-system-alist '("\\.dyalog\\'" . utf-8))
-;; Make fill-paragraph work only in comment blocks and respect
-;; Profdoc doc comments.
-(defvar profdoc-doc-comment
-  "\\(ret\\|rarg\\|larg\\|author\\|see\\|docs\\|group\\|ex\\)")
-(add-hook 'dyalog-mode-hook
-          '(lambda ()
-             (setq paragraph-start
-                   (concat "^\\s-*⍝\\s-*\\\\" profdoc-doc-comment ".*$"))
-             (which-func-mode t)))
-(autoload 'dyalog-ediff-forward-word "dyalog-mode" "Dyalog mode" t)
-(setq ediff-forward-word-function 'dyalog-ediff-forward-word)
+;; Dyalog-mode, from https://bitbucket.org/harsman/dyalog-mode
+(add-to-list 'load-path "~/src/dyalog/mode")
+(use-package dyalog-mode
+  :init
+  (progn
+    (add-to-list 'auto-mode-alist '("\\.apl\\'" . dyalog-mode))
+    (add-to-list 'auto-mode-alist '("\\.dyalog$" . dyalog-mode))
+    (add-to-list 'file-coding-system-alist '("\\.apl\\'" . utf-8))
+    (add-to-list 'file-coding-system-alist '("\\.dyalog\\'" . utf-8))
+    ;; Make fill-paragraph work only in comment blocks and respect
+    ;; Profdoc doc comments.
+    (defvar profdoc-doc-comment
+      "\\(ret\\|rarg\\|larg\\|author\\|see\\|docs\\|group\\|ex\\)")
+    (add-hook 'dyalog-mode-hook
+              '(lambda ()
+                 (setq paragraph-start
+                       (concat "^\\s-*⍝\\s-*\\\\" profdoc-doc-comment ".*$"))
+                 (which-func-mode t)))
+    (autoload 'dyalog-ediff-forward-word "dyalog-mode" "Dyalog mode" t)
+    (setq ediff-forward-word-function 'dyalog-ediff-forward-word)))
