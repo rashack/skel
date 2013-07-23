8ecc1d951dd2ded7e8247a814b24927cfce8fc17
Kjell Andreassen <andreassen.kjell@gmail.com>
2013-07-23 18:35:41 +0200

    Haskell mode has been updated.

--- elisp/my-haskell.el
+++ elisp/my-haskell.el
@@ -1,7 +1,10 @@
-(load-and-do-if-exists
- "~/src/haskell-mode/haskell-site-file"
- (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
- (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
- ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
- ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
- )
+(add-to-list 'load-path "~/src/haskell-mode/")
+(use-package haskell-mode-autoloads
+  :init
+  (progn
+    (add-to-list 'Info-default-directory-list "~/lib/emacs/haskell-mode/")
+    (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+    (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+    ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+    ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+))
