f88f3cfb71913f8b75b4968782db63996c45ceb0
Kjell Andreassen <andreassen.kjell@gmail.com>
2013-08-27 01:51:44 +0200

    Add first logged in and last logged out time. Pad hours with prefix 0 if < 10 hours.

--- bin/time.py
+++ bin/time.py
@@ -19,9 +19,24 @@ def day (t0):
 def timediffstr (t0, t1):
     return datetime (*t1[0:6]) - datetime (*t0[0:6])
 
-def print_day_sum (t0, daytime):
-    thedate = date.fromtimestamp (time.mktime (t0))
-    print thedate.strftime ('%Y-%m-%d'), timedelta (seconds=daytime)
+def timedeltastring (td):
+    hours, remainder = divmod (td.seconds, 3600)
+    minutes, seconds = divmod (remainder, 60)
+    return '%02d:%02d:%02d' % (hours, minutes, seconds)
+
+def ts2datestr (timestruct):
+    return date.fromtimestamp (time.mktime (t0)).strftime ('%Y-%m-%d')
+
+def ts2timestr (ts):
+    return datetime.fromtimestamp (time.mktime (ts)).strftime ('%H:%M:%S')
+
+def daytimes (ts0, ts1):
+    return '[' + ts2timestr (ts0) + ' - ' + ts2timestr (ts1) + ']'
+
+def print_day_sum (t0, daytime, daystart, dayend):
+    thedate = date.fromtimestamp (time.mktime (t0)).strftime ('%Y-%m-%d')
+    totaltime = timedeltastring (timedelta (seconds=daytime))
+    print thedate, totaltime, daytimes (daystart, dayend)
 
 f = open(argv[1], 'r')
 line = f.readline ().split ()
@@ -29,6 +44,7 @@ if line[1] == 'OUT': # If the first line of the file is an OUT, ignore it.
     line = f.readline ().split ()
 prev = None
 daytime = 0
+daystart = str2time (line[0])
 while line:
     if prev is not None:
         t0 = str2time (prev[0])
@@ -37,20 +53,25 @@ while line:
         else:
             t1 = time.localtime ()
             print t1[2]
-        if day (t0) != day (t1):
-            if line[1] == 'OUT':
+        if day (t0) != day (t1): # new day
+            if line[1] == 'OUT': # stayed logged in past midnight
                 daytime += time_diff (t0, (date (t0[0], t0[1],
                                                  t0[2]) + timedelta (days=1)).timetuple ())
-                print_day_sum (t0, daytime)
-                # if logged in over midnight start at OUT time instead of 0
+                dayend = date (t1[0], t1[1], t1[2]).timetuple ()
+                print_day_sum (t0, daytime, daystart, dayend)
+                # since logged in past midnight, start at OUT time instead of 0
                 daytime = time_diff (date (t1[0], t1[1], t1[2]).timetuple (), t1)
+                daystart = date (t1[0], t1[1], t1[2]).timetuple ()
             else:
-                print_day_sum (t0, daytime)
+                dayend = t0
+                print_day_sum (t0, daytime, daystart, dayend)
                 daytime = 0
+                daystart = t1
         elif prev[1] == 'IN':
             daytime += time_diff (t0, t1)
     prev = line
     line = f.readline ().split ()
-if prev[1] == 'IN':
+if prev[1] == 'IN': # still logged in it seems, use present time
     daytime += time_diff (t1, time.localtime ())
-print_day_sum (t0, daytime)
+    t1 = time.localtime ()
+print_day_sum (t0, daytime, daystart, t1)
