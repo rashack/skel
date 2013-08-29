b60985ed611ddc8b34326fecb34c969f4ca15740
Kjell Andreassen <andreassen.kjell@gmail.com>
2013-08-29 15:40:03 +0200

    Add some error handling, and don't try to mount already mounted devices. Add some colour to it.

---
+++ bin/colours.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export COLR=$(tput setaf 1) # red
+export COLG=$(tput setaf 2) # green
+export COLY=$(tput setaf 3) # yellow
+export COLB=$(tput setaf 4) # blue
+export COLM=$(tput setaf 5) # magenta
+export COLC=$(tput setaf 6) # cyan
+export COLN=$(tput sgr0)    # normal
