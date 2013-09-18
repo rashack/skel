ce6546274aaff11084cff972910ab84ac6f0303b
Kjell Andreassen <andreassen.kjell@gmail.com>
2013-09-18 09:03:19 +0200

    Check if ssh key is added to ssh-agen, add it if not. Why do I need to do 'eval $1' instead of just '$1' to get ssh-add to work?

--- bin/gspp.sh
+++ bin/gspp.sh
@@ -7,13 +7,29 @@ NORMAL=$(tput sgr0)
 
 run_command () {
     echo "$YELLOW$1$NORMAL" >& 2
-    $1
+    eval $1
     ERR=$?
     if [ $ERR -ne 0 ] ; then
         echo "$1: ${RED}command failed with exit code $ERR$NORMAL"
         exit $?
     fi
 }
+
+check_add_ssh_key () {
+    GIT_HOST=$(cat .git/config | grep "url \?= \?ssh://" | perl -pe 's|\s*url *= *ssh://([^/]+)/.*|$1|')
+    echo $GIT_HOST
+    if [ ! $(ssh-add -l | grep -q $GIT_HOST) ] ; then
+        echo "${YELLOW}Couldn't find $GIT_HOST in ssh-agent, trying to add.${NORMAL}"
+        HOST_KEY="~/.ssh/$GIT_HOST"
+        if [ -f $HOST_KEY ] ; then
+            echo "${RED}Couldn't find $HOST_KEY, giving up.${NORMAL}"
+            exit 1;
+        fi
+        run_command "ssh-add $HOST_KEY"
+    fi
+}
+
+check_add_ssh_key
 
 git diff --quiet
 STASH_NEEDED=$?
