ea34c1da10a6df051a2aec7dfb58c10b0c2458d4
Kjell Andreassen <andreassen.kjell@gmail.com>
2013-11-16 01:13:47 +0100

    Better path handling.

--- elisp/my-haskell.el
+++ elisp/my-haskell.el
@@ -1,8 +1,8 @@
-(add-to-list 'load-path (elisp-root "haskell-mode/"))
+(add-to-list 'load-path (elisp-root "haskell-mode"))
 (use-package haskell-mode-autoloads
   :init
   (progn
-    (add-to-list 'Info-default-directory-list "~/lib/emacs/haskell-mode/")
+    (add-to-list 'Info-default-directory-list (elisp-root "haskell-mode"))
     (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
     (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
     ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
