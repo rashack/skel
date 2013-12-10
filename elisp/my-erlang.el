98229b19cdccbdae1b32dd57a6e9a72fde005205
Kjell Andreassen <andreassen.kjell@gmail.com>
2013-12-10 17:38:09 +0100

    Function to transform an Erlang stacktrace to something digestable.

--- elisp/my-erlang.el
+++ elisp/my-erlang.el
@@ -11,3 +11,17 @@
                 (setq-local whitespace-style '(face lines-tail))
                 (setq-local whitespace-line-column 80)
                 (whitespace-mode t)))))
+
+(defun erl-trace (start end)
+  "Try to convert an Erlang stack trace to something a little more readable."
+  (interactive "*r")
+  (save-excursion
+    (save-restriction
+      (narrow-to-region start end)
+      ;; rewrite "<module>, <function>, <arity>, [{file" to "<module>:<function>/<arity>, [{file"
+      (replace-regexp "\\(\\(\\sw\\|\\s_\\)+\\),[ \t\n]*\\(\\(\\sw\\|\\s_\\)+\\),[ \t\n]*\\([0-9]\\),[ \t\n]*\\(\\[{file\\)"
+                      "\\1:\\3/\\5, \\6" nil (point-min) (point-max))
+      ;; rewrite "[{file, "<file>"}, {line, <line>}]" to "<file>:<line>"
+      (replace-regexp "\\[{file,[ \t\n]*\"\\([^\"]*\\)\"},[ \t\n]*{line,[ \t\n]*\\([0-9]+\\)}\\]"
+                      "\"\\1:\\2\"" nil (point-min) (point-max))
+      (align-regexp (point-min) (point-max) ".*\\(, +\\).*" 1 2 t))))
