d45152c5496776eefc3429fd5cc21c8ed783e5d2
Kjell Andreassen <andreassen.kjell@gmail.com>
2014-04-09 12:57:54 +0200

    Pad binary output with zeros (up to a byte).

--- bin/bin2hex.sh
+++ bin/bin2hex.sh
@@ -10,12 +10,22 @@ fi
 
 if [[ $0 =~ "2bin" ]] ; then
     OBASE="obase=2"
+    BINARY_OUT=t
 elif [[ $0 =~ "2dec" ]] ; then
     OBASE="obase=10"
 elif [[ $0 =~ "2hex" ]] ; then
     OBASE="obase=16"
 fi
 
+convert() {
+    local RES=$(echo "$OBASE;$IBASE;$(echo $1 | tr [a-z] [A-Z])" | bc)
+    if [ -z $BINARY_OUT ] ; then
+        echo $RES
+    else
+        printf "%0.8d" $RES
+    fi
+}
+
 for x in "$@" ; do
-    echo "$x: "$(echo "$OBASE;$IBASE;$(echo $x | tr [a-z] [A-Z])" | bc)
+    echo "$x: "$(convert $x)
 done
