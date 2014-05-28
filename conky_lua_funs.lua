015adb77477ffe1abd260e43651b61b207eeaf23
Kjell Andreassen <andreassen.kjell@gmail.com>
2014-05-28 23:56:22 +0200

    Colorize the battery percentage in the dzen/conky status bar with Lua.

--- conky_lua_funs.lua
+++ conky_lua_funs.lua
@@ -1,14 +1,28 @@
 
 function conky_lc(what, string)
-    value=tonumber(conky_parse("${" .. what .. "}"))
-    if value >= 70 then
-        fg_colour=0xff0000
-    elseif value >= 20 then
-        fg_colour=0xff7f00
-    elseif value >= 15 then
-        fg_colour=0xffff00
-    else
-        fg_colour=0x00ff00
-    end
-    return string.format("%s: ^fg(#%06x)%d^fg()", string, fg_colour, value)
+   value=tonumber(conky_parse("${" .. what .. "}"))
+   if value >= 70 then
+      fg_colour=0xff0000
+   elseif value >= 20 then
+      fg_colour=0xff7f00
+   elseif value >= 15 then
+      fg_colour=0xffff00
+   else
+      fg_colour=0x00ff00
+   end
+   return string.format("%s: ^fg(#%06x)%d^fg()", string, fg_colour, value)
+end
+
+function conky_lc_n(what, string)
+   value=tonumber(conky_parse("${" .. what .. "}"))
+   if value >= 60 then
+      fg_colour=0x0000ff
+   elseif value >= 50 then
+      fg_colour=0x7fff00
+   elseif value >= 15 then
+      fg_colour=0xffff00
+   else
+      fg_colour=0xff0000
+   end
+   return string.format("%s: ^fg(#%06x)%d^fg()", string, fg_colour, value)
 end
