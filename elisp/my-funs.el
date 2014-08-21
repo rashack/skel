8f0c5a000cb78207ba8b90f03dc1e99800b97fd2
Kjell Andreassen <andreassen.kjell@gmail.com>
2014-08-21 11:12:34 +0200

    Show diff for two regions.

--- elisp/my-funs.el
+++ elisp/my-funs.el
@@ -330,3 +330,28 @@ sPut a space on both sides of: ")
     (perform-replace (concat non-space-group regexp non-space-group)
                      (concat "\\1 " regexp " \\2")
                      t t nil nil nil start end)))
+
+(defun diff-region ()
+  "Select a region to compare"
+  (interactive)
+  (when (use-region-p) ; there is a region
+    (let (buf)
+      (setq buf (get-buffer-create "*Diff-regionA*"))
+      (save-current-buffer
+        (set-buffer buf)
+        (erase-buffer))
+      (append-to-buffer buf (region-beginning) (region-end))))
+  (message "Now select other region to compare and run `diff-region-now`"))
+
+(defun diff-region-now ()
+  "Compare current region with region already selected by `diff-region`"
+  (interactive)
+  (when (use-region-p)
+    (let (bufa bufb)
+      (setq bufa (get-buffer-create "*Diff-regionA*"))
+      (setq bufb (get-buffer-create "*Diff-regionB*"))
+      (save-current-buffer
+        (set-buffer bufb)
+        (erase-buffer))
+      (append-to-buffer bufb (region-beginning) (region-end))
+      (ediff-buffers bufa bufb))))
