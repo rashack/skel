ee8a6b166dacf5eabb3cc5b7a9389cb41a1f189f
Kjell Andreassen <andreassen.kjell@gmail.com>
2014-09-26 21:47:27 +0200

    Clojure/cider config.

--- elisp/my-clojure.el
+++ elisp/my-clojure.el
@@ -3,4 +3,8 @@
 (add-to-list 'load-path (elisp-root "clojure-mode"))
 (use-package clojure-mode)
 
+(add-to-list 'load-path (expand-file-name "elpa/queue-0.1.1" my-emacs-dir))
+(require 'queue)
 (my-load-use 'cider)
+(setq cider-repl-history-size 1000)
+(setq cider-repl-history-file (expand-file-name "cider-repl-history" my-emacs-dir))
