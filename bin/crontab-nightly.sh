c7a4d92b04addd769553afa17ad9f4346de3a8a8
Kjell Andreassen <andreassen.kjell@gmail.com>
2014-10-12 02:24:19 +0200

    One backup after the other.

---
+++ bin/crontab-nightly.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+/home/kjell/bin/backup /home/kjell /mnt/backup/kjell
+/home/kjell/bin/backup /mnt/kjell /mnt/backup/kjell
+/home/kjell/bin/backup_remote tp /home/kjell /home/kjell/.ssh/tp-backup
