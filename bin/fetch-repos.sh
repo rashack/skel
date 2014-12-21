e81812e90e13e229cfe8404189cd80291a490a10
Kjell Andreassen <andreassen.kjell@gmail.com>
2014-12-21 19:51:55 +0100

    Fetch, clone and show status for repos from fetch-repos script.

--- bin/fetch-repos.sh
+++ bin/fetch-repos.sh
@@ -1,8 +1,75 @@
 #!/bin/bash
 
 source ~/bin/try-run.sh
+source ~/.repos-to-fetch
 
-while read repo ; do
-    try_run "cd ~/$repo"
-    git fetch --all
-done < ~/.repos-to-fetch
+repo_name() {
+    echo ${REPOS[$(( $1 * 3 + 0 ))]}
+}
+
+repo_dir() {
+    echo ${REPOS[$(( $1 * 3 + 1 ))]}
+}
+
+repo_uri() {
+    echo ${REPOS[$(( $1 * 3 + 2 ))]}
+}
+
+fetch_repos() {
+    for (( i=0 ; i < ${#REPOS[@]} / 3 ; i=$i+1 )) ; do
+        REPO_DIR=$(repo_dir $i)
+        try_run "cd ~/$REPO_DIR"
+        try_run "git fetch --all"
+    done
+}
+
+clone_repos() {
+    for (( i=0 ; i < ${#REPOS[@]} / 3 ; i=$i+1 )) ; do
+        REPO_DIR=$(repo_dir $i)
+        REPO_URI=$(repo_uri $i)
+        REPO_NAME=$(repo_name $i)
+        try_run "cd ~/$(dirname $REPO_DIR)"
+        if [ -d $(basename $REPO_DIR) ] ; then
+            continue
+        fi
+        try_run "git clone $REPO_URI $REPO_NAME"
+    done
+}
+
+shortstat_repos() {
+    for (( i=0 ; i < ${#REPOS[@]} / 3 ; i=$i+1 )) ; do
+        REPO_DIR=$(repo_dir $i)
+        REPO_URI=$(repo_uri $i)
+        REPO_NAME=$(repo_name $i)
+        if [ -d ~/$REPO_DIR ] ; then
+            try_run "cd ~/$REPO_DIR"
+            git status -sb | head -1
+        else
+            echo "$REPO_NOE: not cloned"
+        fi
+    done
+}
+
+usage() {
+    echo "usage: $(basename $0) OPTION
+
+    OPTION
+        -c    clone repos and fetch existing ones
+        -f    fetch existing repos
+        -s    print short status for repos
+"
+}
+
+if [ $# -lt 1 ] ; then
+    usage
+    exit 1
+fi
+
+while getopts "cfs" opt ; do
+    case $opt in
+        c) clone_repos ;;
+        f) fetch_repos ;;
+        s) shortstat_repos ;;
+    esac
+done
+shift `expr $OPTIND - 1`
