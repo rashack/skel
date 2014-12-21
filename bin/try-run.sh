5a4213171780c9bd097f6865036c3c381d103b8e
Kjell Andreassen <andreassen.kjell@gmail.com>
2014-12-21 13:47:51 +0100

    A fancy version of try_run

--- bin/try-run.sh
+++ bin/try-run.sh
@@ -12,3 +12,24 @@ try_run () {
         exit $ERR
     fi
 }
+
+try_run_fancy() {
+    if (( $# > 1 )) ; then
+        local CMD=${@:2}
+        echo "$COLY$CMD$COLN" >& 2
+        local WIDTH=$(( $(tput cols) + 8 ))
+    else
+        local CMD=$@
+        echo -n "$COLY$CMD$COLN" >& 2
+        local WIDTH=$(( $(tput cols) - ${#CMD} + 8 ))
+    fi
+
+    eval $CMD
+    ERR=$?
+    if [ $ERR -ne 0 ] ; then
+        echo "$CMD: ${COLR}command failed with exit code $ERR$COLN" >& 2
+        exit $ERR
+    else
+        printf "%${WIDTH}s\n" "[ ${COLG}OK${COLN} ]" >& 2
+    fi
+}
