98c0d59cf6de1b193050501aac333d9be2e01783
Kjell Andreassen <andreassen.kjell@gmail.com>
2015-01-18 14:08:20 +0100

    Script for backing up the backup disk.
    Checking for different drives every other week.

---
+++ bin/backup-weekly.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+RSYNC_OPTS="--archive --one-file-system --hard-links --human-readable \
+--inplace --numeric-ids --delete --delete-excluded --exclude-from=$EXCLUDES"
+BACKUP_SOURCE=/mnt/backup/
+BACKUP_DEST=/mnt/weekly
+
+run_command () {
+    eval $1
+    ERR=$?
+    if [ $ERR -ne 0 ] ; then
+        echo "$1: command failed with exit code $ERR"
+        exit $?
+    fi
+}
+
+week_parity () {
+    echo "scale=0; $(date +%V) % 2" | bc
+}
+
+run_command "mount LABEL=backup-$(week_parity) /mnt/weekly"
+run_command "rsync $RSYNC_OPTS $BACKUP_SOURCE $BACKUP_DEST"
