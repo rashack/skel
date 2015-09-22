f8fa09c9a10d14cd42c0a9d2766ff30bfcb5d355
Kjell Andreassen <andreassen.kjell@gmail.com>
2015-09-22 13:40:55 +0200

    A little more stable 0xdbe start script.

--- bin/0xdbe.sh
+++ bin/0xdbe.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-LATEST=$(ls -d1 ~/Downloads/0xDBE* | tail -1)
+latest() {
+    local latest=$(find ~/Downloads/ -maxdepth 1 -type d -name 0xDBE\* | sort | tail -1)
+    $latest/bin/0xdbe.sh
+}
 
-$LATEST/bin/0xdbe.sh
+latest
