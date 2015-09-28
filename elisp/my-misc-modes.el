6a61880d3fbcc708ebbee7a94595beee82ede3ee
Kjell Andreassen <andreassen.kjell@gmail.com>
2015-09-29 00:18:27 +0200

    I'll find a suitable flex mode if I start using it again.

--- elisp/my-misc-modes.el
+++ elisp/my-misc-modes.el
@@ -1,6 +1,4 @@
 (my-load-use 'markdown-mode)
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-(my-load-use 'flex-mode)
-(add-to-list 'auto-mode-alist '("\\.flex\\'" . flex-mode))
 
 (require 'dos)
