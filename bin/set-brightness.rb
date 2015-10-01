4d2cee3f35a283225c39a1e6e7257ce574df66d4
Kjell Andreassen <andreassen.kjell@gmail.com>
2015-10-02 00:53:32 +0200

    Better brightness logic.

--- bin/set-brightness.rb
+++ bin/set-brightness.rb
@@ -4,8 +4,16 @@ MAX = %x{cat /sys/class/backlight/intel_backlight/max_brightness}.to_f
 CURR = %x{cat /sys/class/backlight/intel_backlight/brightness}.to_f
 CURR_PERC = (CURR / MAX * 100).round.to_i
 
-TO = ARGV[0].to_i
+def get_to(prev, change)
+  return change.to_i if /^\d+$/ =~ change
+  /^(?<op>[+-])(?<val>\d+)/ =~ change
+  return prev + val.to_i if op == "+"
+  return prev - val.to_i if op == "-"
+  return prev
+end
+to = get_to(CURR_PERC, ARGV[0])
+to = 1 if to < 1
 
-NEW = (MAX * TO / 100).round
-puts "Setting brightness to #{TO}% (#{NEW}/#{MAX.to_i}), brightness was #{CURR_PERC}% (#{CURR}/#{MAX.to_i})"
-%x{echo #{NEW} | sudo tee /sys/class/backlight/intel_backlight/brightness}
+new = (MAX * to / 100).round
+puts "Setting brightness to #{to}% (#{new}/#{MAX.to_i}), brightness was #{CURR_PERC}% (#{CURR}/#{MAX.to_i})"
+%x{echo #{new} | sudo tee /sys/class/backlight/intel_backlight/brightness}
