29345e9140ddfa730372c2e8d9854c6a6cf0aec6
Kjell Andreassen <andreassen.kjell@gmail.com>
2016-12-27 16:38:22 +0100

    Updated twrp backup pulling

--- bin/android-pull-twrp-backups.sh
+++ bin/android-pull-twrp-backups.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
+set -eu
+
 LOCAL_BACKUP_ROOT=~/android/backup
 REMOTE_BACKUP_ROOT=/sdcard/TWRP/BACKUPS
 
 # because stupid twrp names a directory with spaces
 IFS='
 '
+
+pull_backup() {
+    echo "Starting pull from $REMOTE_BACKUP to $LOCAL_BACKUP"
+    adb pull $REMOTE_BACKUP
+}
+
 # tr -d '\r' is needed because stupid 'adb shell ls' sends \r\n as EOL
-for DIRS in $(adb shell ls $REMOTE_BACKUP_ROOT | tr -d '\r') ; do
-    for BACKUPS in $(adb shell ls $REMOTE_BACKUP_ROOT/$DIRS | tr -d '\r') ; do
-        LOCAL_BACKUP=$LOCAL_BACKUP_ROOT/$DIRS/$BACKUPS
-        REMOTE_BACKUP=$REMOTE_BACKUP_ROOT/$DIRS/$BACKUPS
-        if [ -d $LOCAL_BACKUP ] ; then
-            echo "Backup $LOCAL_BACKUP already exists, skipping."
-            continue
-        fi
-        mkdir -p $LOCAL_BACKUP
-        echo "Starting pull from $REMOTE_BACKUP to $LOCAL_BACKUP"
-        for FILES in $(adb shell ls $REMOTE_BACKUP | tr -d '\r') ; do
-            echo "files = $FILES"
-            FILE=$REMOTE_BACKUP/$FILES
-            echo "pulling $FILE"
-            adb pull $FILE $LOCAL_BACKUP
-        done
-    done
+DEVICE_BACKUP_DIR=$(adb shell ls $REMOTE_BACKUP_ROOT | tr -d '\r')
+for BACKUP in $(adb shell ls $REMOTE_BACKUP_ROOT/$DEVICE_BACKUP_DIR | tr -d '\r') ; do
+    LOCAL_BACKUP=$LOCAL_BACKUP_ROOT/$DEVICE_BACKUP_DIR
+    REMOTE_BACKUP=$REMOTE_BACKUP_ROOT/$DEVICE_BACKUP_DIR/$BACKUP
     cd $LOCAL_BACKUP
+    if [ -d "$LOCAL_BACKUP/$BACKUP" ] ; then
+        echo "Backup $LOCAL_BACKUP already exists, skipping."
+    else
+        pull_backup
+    fi
+    echo "Checking MD5 sums"
+    cd $BACKUP
     md5sum -c *.md5
 done
