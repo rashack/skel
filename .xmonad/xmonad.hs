8c0b9670fd89adfe4849e48441ff65573b555bc0
Kjell Andreassen <andreassen.kjell@gmail.com>
2019-03-09 12:49:29 +0100

    Replace deprecated functions and untabify

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,10 +1,10 @@
 import Data.Bits
+import Data.Default (def)
 import Graphics.X11
 import System.IO (hPutStrLn)
 import XMonad
 import XMonad.Actions.CycleWS
 import XMonad.Actions.NoBorders
-import XMonad.Config (defaultConfig)
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.DynamicLog hiding (xmobar)
 import XMonad.Hooks.EwmhDesktops
@@ -39,8 +39,8 @@ myLayout = gaps [(U,18)] (smartBorders (tiled ||| full ||| threeCol ||| resizabl
              full            = toggleLayouts Full (tabbed shrinkText myTabConfig)
              resizableTall   = ResizableTall 1 (3/100) (1/2) []
 
-myTabConfig = defaultTheme {   activeBorderColor = "#FF9900",   activeColor = "#444444",   activeTextColor = "#00CC00"
-                           , inactiveBorderColor = "#444444", inactiveColor = "#222222", inactiveTextColor = "#888888"}
+myTabConfig = def {   activeBorderColor = "#FF9900",   activeColor = "#444444",   activeTextColor = "#00CC00"
+                  , inactiveBorderColor = "#444444", inactiveColor = "#222222", inactiveTextColor = "#888888"}
 
 main :: IO ()
 main = do
@@ -48,32 +48,32 @@ main = do
         spawn "~/bin/dzen2-time"
         spawn "~/bin/dzen2-idle"
         spawn "~/bin/dzen2-load"
-        xmonad (ewmh $ withUrgencyHook NoUrgencyHook defaultConfig)
+        xmonad (ewmh $ withUrgencyHook NoUrgencyHook def)
          {
            --dzen2
-           logHook = dynamicLogWithPP $ defaultPP { ppOutput = hPutStrLn h
-                                                  , ppCurrent = wrapFg "yellow" . wrap "[" "]"
-                                                  , ppVisible = wrap "(" ")"
-                                                  , ppSep = wrapFg "grey" " | "
-                                                  , ppTitle = (\x -> wrapFg myTitleFgColor x)
-                                                  , ppUrgent = dzenColor "dark orange" "" .  dzenStrip
-                                                  }
-	 , normalBorderColor  = "#334455"
+           logHook = dynamicLogWithPP $ def { ppOutput = hPutStrLn h
+                                            , ppCurrent = wrapFg "yellow" . wrap "[" "]"
+                                            , ppVisible = wrap "(" ")"
+                                            , ppSep = wrapFg "grey" " | "
+                                            , ppTitle = (\x -> wrapFg myTitleFgColor x)
+                                            , ppUrgent = dzenColor "dark orange" "" .  dzenStrip
+                                            }
+         , normalBorderColor  = "#334455"
          , focusedBorderColor = "#ff9900"
          , terminal           = "~/bin/xmterm"
          , modMask = mod4Mask
-	 , workspaces = myWorkspaces
+         , workspaces = myWorkspaces
          , layoutHook = myLayout
          , manageHook = composeAll [ className =? "fontforge" --> doFloat
                                    , className =? "stalonetray" --> doIgnore
                                    , className =? "Gimp"      --> doFloat
-				   , className =? "sun-applet-Main" --> doFloat
-				   , className =? "sun.applet.Main" --> doFloat
-				   , className =? "vmware-server-console" --> doFloat
-				   , className =? "Vmware-server-console" --> doFloat
-				   , className =? "sun-plugin-navig-motif-Plugin" --> doFloat
-				   ]
-         , keys = \c -> mykeys c `M.union` keys defaultConfig c
+                                   , className =? "sun-applet-Main" --> doFloat
+                                   , className =? "sun.applet.Main" --> doFloat
+                                   , className =? "vmware-server-console" --> doFloat
+                                   , className =? "Vmware-server-console" --> doFloat
+                                   , className =? "sun-plugin-navig-motif-Plugin" --> doFloat
+                                   ]
+         , keys = \c -> mykeys c `M.union` keys def c
          , startupHook = setWMName "LG3D" -- for swing applications
          , focusFollowsMouse = True
          }
@@ -95,18 +95,18 @@ main = do
              , ((modm, xK_i), spawn "~/bin/passmenu")
              , ((modm, xK_o), spawn "PASSWORD_STORE_DIR=~/.pass-store-w ~/bin/passmenu")
              , ((modm, xK_s), spawn "~/bin/screenshot.sh")
-	     ]
-	     ++
+             ]
+             ++
              [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-	          | (key, sc) <- zip [xK_w, xK_e, xK_r] [0, 1, 2]
+                  | (key, sc) <- zip [xK_w, xK_e, xK_r] [0, 1, 2]
              , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]] ++
-	     [ ((modm, k), windows $ W.greedyView i)
-	         | (i, k) <- zip myWorkspaces workspaceKeys
-	     ] ++
-	     [ ((modm .|. shiftMask, k), (windows $ W.shift i))
-	         | (i, k) <- zip myWorkspaces workspaceKeys
-	     ]
-	     where workspaceKeys = [xK_1, xK_2, xK_3, xK_4, xK_5, xK_6, xK_7, xK_8, xK_9, xK_0, xK_plus]
+             [ ((modm, k), windows $ W.greedyView i)
+                 | (i, k) <- zip myWorkspaces workspaceKeys
+             ] ++
+             [ ((modm .|. shiftMask, k), (windows $ W.shift i))
+                 | (i, k) <- zip myWorkspaces workspaceKeys
+             ]
+             where workspaceKeys = [xK_1, xK_2, xK_3, xK_4, xK_5, xK_6, xK_7, xK_8, xK_9, xK_0, xK_plus]
      myRestart  = "for pid in `pgrep conky`; do kill -9 $pid; done && " ++
                   "for pid in `pgrep dzen2`; do kill -9 $pid; done && " ++
                   "xmonad --recompile && xmonad --restart"
