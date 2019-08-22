369c003ce2dbc400da41591d95ee3428753c6f9f
Kjell Andreassen <andreassen.kjell@gmail.com>
2019-08-22 10:11:57 +0200

    Update intellij start script idea.sh

--- bin/idea.sh
+++ bin/idea.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
+set -eu
+
+usage() {
+    echo "usage: $0 [-c|-u]"
+    echo
+    echo "        -c IntelliJ Community Edition"
+    echo "        -u IntelliJ Ultimate Edition"
+}
+
 IDEA_INSTALLATIONS=$HOME/Downloads
 
-LATEST=$(ls -1d $IDEA_INSTALLATIONS/idea-IU* | tail -1)
+IDEA_IC="idea-IC*"
+IDEA_IU="idea-IU*"
+FLAVOUR="$IDEA_IC"
+
+if [ $# != 1 ] ; then
+    usage
+    exit 1
+fi
+
+while getopts "cu" opt ; do
+    case $opt in
+        c) FLAVOUR="$IDEA_IC" ;;
+        u) FLAVOUR="$IDEA_IU*" ;;
+    esac
+done
+shift `expr $OPTIND - 1`
+
+
+LATEST=$(ls -1d $IDEA_INSTALLATIONS/$FLAVOUR | tail -1)
 
 IDEA=$LATEST/bin/idea.sh
 echo "Staring InjtelliJ IDEA: $IDEA"
