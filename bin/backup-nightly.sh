938d8d9453ff523773bb1baf6dc4cdd9f57cb080
Kjell Andreassen <andreassen.kjell@gmail.com>
2020-07-25 17:00:02 +0200

    Fix small backup issies

--- bin/backup-nightly.sh
+++ bin/backup-nightly.sh
@@ -8,6 +8,6 @@ case $(hostname) in
         ;;
     harc)
         /home/kjell/bin/backup /home/kjell /mnt/backup/kjell
-        /home/kjell/bin/backup /mnt/kjell /mnt/backup/kjell
+        # /home/kjell/bin/backup /mnt/kjell /mnt/backup1/kjell
         ;;
 esac
