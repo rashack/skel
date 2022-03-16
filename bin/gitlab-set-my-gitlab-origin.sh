49c062893ff33467562e6217fc5bb363ae431fad
Kjell Andreassen <andreassen.kjell@gmail.com>
2022-03-16 14:18:47 +0100

    Add dry run option '-d'

--- bin/gitlab-set-my-gitlab-origin.sh
+++ bin/gitlab-set-my-gitlab-origin.sh
@@ -2,13 +2,27 @@
 
 set -euo pipefail
 
+DRY_RUN=n
+while getopts "d" opt ; do
+    case $opt in
+        d)
+            set -x
+            DRY_RUN=y ;;
+    esac
+done
+shift `expr $OPTIND - 1`
+
 unset NEW_URL
 unset REMOTE
 REMOTE=$(git remote get-url origin)
 NEW_URL=$(echo $REMOTE | perl -pe 's/git\@git(lab|hub).com:/ssh:\/\/my-git\1\//g')
 
 if [ -n "$NEW_URL" ] ; then
-    git remote set-url origin "$NEW_URL"
+    if [ "x$DRY_RUN" = "xy" ] ; then
+        echo "Dry run: 'git remote set-url origin $NEW_URL'"
+    else
+        git remote set-url origin "$NEW_URL"
+    fi
 else
     echo "Not a gitlab repo: $REMOTE"
     exit 1
