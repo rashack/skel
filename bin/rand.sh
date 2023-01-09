7b04faa9756e121691c00721172acd07b1b6aa55
Kjell Andreassen <andreassen.kjell@gmail.com>
2023-01-09 04:02:05 +0100

    Get some randomness with selectable character classes

--- bin/rand.sh
+++ bin/rand.sh
@@ -1,3 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
-head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9~!@#$%^&*_-' | fold -w 16
+set -euo pipefail
+
+BASE64=false
+N_CHARS=16
+
+while getopts "Aabl:nsw:" opt ; do
+    case $opt in
+        A) CHARS="" ;;
+        a) CHARS="${CHARS-}a-zA-Z" ;; # 50
+        b) BASE64=true ;;
+        l) N_CHARS=$OPTARG ;;
+        n) CHARS="${CHARS-}0-9" ;; # 10
+        s) CHARS="${CHARS-}~!@#$%^&*_-" ;; # 11
+        w) WIDTH=$OPTARG
+    esac
+done
+shift `expr $OPTIND - 1`
+
+CHARS=${CHARS-:'a-zA-Z0-9~!@#$%^&*_-'}
+
+LC_ALL=C
+
+if [ $BASE64 = "true" ] ; then
+    head -c $N_CHARS /dev/urandom | base64
+else
+    head -c 500 /dev/urandom | tr -dc "$CHARS" | fold -w "$N_CHARS"
+fi
+echo
