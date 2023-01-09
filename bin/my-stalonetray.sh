a13fc8194b755f547468a3897497454cbc269525
Kjell Andreassen <andreassen.kjell@gmail.com>
2023-01-09 04:12:40 +0100

    Log stalonetray start/activity

--- bin/my-stalonetray.sh
+++ bin/my-stalonetray.sh
@@ -7,4 +7,7 @@ source ~/bin/dzen2-env
 SIZE=16
 X_POS=$(( $SCREEN_WIDTH - $SIZE ))
 
-stalonetray --icon-size=$SIZE --geometry 1x1+$X_POS+0 --grow-gravity SE &
+ts=$(date +'%F %T')
+echo "$ts starting stalonetray: exec stalonetray --icon-size=$SIZE --geometry 1x1+$X_POS+0 --grow-gravity SE" 2>&1 >> ~/.dzen2.log
+
+exec stalonetray --icon-size=$SIZE --geometry 1x1+$X_POS+0 --grow-gravity SE 2>&1 >> ~/.dzen2.log
