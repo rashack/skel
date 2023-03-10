abd9fc25ea212623c032e7c839c3ad5f7b316463
Kjell Andreassen <andreassen.kjell@gmail.com>
2023-03-10 13:46:46 +0100

    Support nvme ssd, show power state and colourize a little

--- bin/drives.rb
+++ bin/drives.rb
@@ -3,20 +3,36 @@
 
 require 'text-table'
 
-def model(dev)
+def model_sata(dev)
   %x{ sudo hdparm -I #{dev} | sed -n 's/.*Model Number: \\+\\(.*\\)/\\1/p' }.strip
 end
 
-def serial(dev)
+def model_nvme(dev)
+  %x{ sudo nvme id-ctrl #{dev} | grep '^mn[^a-z]' | awk -F : '{print $2}' }.strip
+end
+
+def serial_sata(dev)
   %x{ sudo hdparm -I #{dev} | sed -n 's/.*Serial Number: \\+\\(.*\\)/\\1/p' }.strip
 end
 
-def label(dev)
+def serial_nvme(dev)
+  %x{ sudo nvme id-ctrl #{dev} | grep '^sn[^a-z]' | awk -F : '{print $2}' }.strip
+end
+
+def label_sata(dev)
   %x{ sudo blkid -s LABEL #{dev} | sed -n 's/.*LABEL="\\([^"]\\+\\)"/\\1/p' }.strip
 end
 
-def uuid(dev)
+def label_nvme(dev)
+  %x{ lsblk -no LABEL #{dev} }.strip
+end
+
+def uuid_sata(dev)
   %x{ sudo blkid -s UUID #{dev} | sed -n 's/.*UUID="\\([^"]\\+\\)"/\\1/p' }.strip
+end
+
+def uuid_nvme(dev)
+  %x{ lsblk -plnf #{dev} | grep '^#{dev}[^a-z0-9]' | awk '{print $4}' }.strip
 end
 
 def mount_point(dev)
@@ -34,11 +50,15 @@ def stats(dev)
 end
 
 def headings
-  ["Dev"] + stats_headings + ["Label", "Model", "Serial", "UUID"]
+  ["Dev"] + stats_headings + ["Label", "Model", "Serial", "UUID", "APM_level", "Power_state"]
 end
 
-def wide(dev)
-  [dev] + stats(dev) + [label(dev), model(dev), serial(dev), uuid(dev)]
+def wide_sata(dev)
+  [dev] + stats(dev) + [label_sata(dev), model_sata(dev), serial_sata(dev), uuid_sata(dev), "", ""]
+end
+
+def wide_nvme(dev)
+  [dev] + stats(dev) + [label_nvme(dev), model_nvme(dev), serial_nvme(dev), uuid_nvme(dev), "", ""]
 end
 
 def devs
@@ -49,19 +69,42 @@ def size(dev)
   %x{ sudo fdisk -l #{dev} | sed -n 's|Disk #{dev}: \\([^,]\\+\\),.*|\\1|p' }.strip.gsub(" ", "").gsub("iB", "")
 end
 
-def partitions(dev)
+def partitions_sata(dev)
   %x{ sudo fdisk -l #{dev} | sed -n 's|^\\(#{dev}[0-9]\\+\\).*|\\1|p' }.split("\n")
+end
+
+def partitions_nvme(dev)
+  %x{ sudo fdisk -l #{dev} | sed -n 's|^\\(#{dev}p[0-9]\\+\\).*|\\1|p' }.split("\n")
+end
+
+def apm_level(dev)
+  %x{ sudo hdparm -B #{dev} | grep APM_level | awk -F= '{print $2}' }.strip
+end
+
+def power_state(dev)
+  %x{ sudo hdparm -C #{dev} | grep "drive state is" | awk -F: '{print $2}' }.strip
 end
 
 def drives
   res = []
+
   %x{ ls /dev/sd? }.split("\n").each do |dev|
-    #      dev     size,       used, avail, use%, mount_point,      label,      model,      serial,      uuid
-    res << [dev] + [size(dev), "",   "",    "",   mount_point(dev), label(dev), model(dev), serial(dev), uuid(dev)]
-    partitions(dev).each do |part|
-      res << wide(part)
+    #      dev            size,       used, avail, use%, mount_point,      label,           model,           serial,           uuid,           apm_level,      power_state
+    res << ["#{dev} *"] + [size(dev), "",   "",    "",   mount_point(dev), label_sata(dev), model_sata(dev), serial_sata(dev), uuid_sata(dev), apm_level(dev), power_state(dev)]
+    #res << ["\e[44m#{dev}\e[0m"] + [size(dev), "",   "",    "",   mount_point(dev), label_sata(dev), model_sata(dev), serial_sata(dev), uuid_sata(dev)]
+    partitions_sata(dev).each do |part|
+      res << wide_sata(part)
     end
   end
+
+    %x{ sudo nvme list | tail +3 | awk '{print $1}' }.split("\n").each do |dev|
+    #      dev            size,       used, avail, use%, mount_point,      label,           model,           serial,           uuid,           apm_level,      power_state
+    res << ["#{dev} *"] + [size(dev), "",   "",    "",   mount_point(dev), label_nvme(dev), model_nvme(dev), serial_nvme(dev), uuid_nvme(dev), "",             ""]
+    partitions_nvme(dev).each do |part|
+      res << wide_nvme(part)
+    end
+  end
+
   res
 end
 
@@ -69,4 +112,4 @@ t2 = Text::Table.new
 t2.head = headings
 t2.rows = []
 t2.rows = drives
-puts t2.to_s
+puts t2.to_s.gsub(/(\/dev\/.+ \*)/, "\e[44m\\1\e[0m")
